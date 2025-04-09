a70b4e42f8b19a1ce1d5e767f062fba709e78cdd
caobaoqi1024 <caobaoqi1024@gmail.com>
2025-04-09 09:43:56 +0800

    :tada: feat(core): init commit

---
+++ src/main/java/dev/mcdd/backend/SpringBoot3QuickstartMvnApplication.java
@@ -0,0 +1,13 @@
+package dev.mcdd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3QuickstartMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3QuickstartMvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mcdd/backend/SpringBoot3QuickstartMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mcdd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3QuickstartMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
